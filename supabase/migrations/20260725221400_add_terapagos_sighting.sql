e5af0f635f84b72924eb78eb82a6bbaf6ed606fd
Annette Yang <annetteyang@Annettes-M1-MacBook-Pro.local>
2026-07-25 15:14:37 -0700

    Add Terapagos UPC sighting

---
+++ supabase/migrations/20260725221400_add_terapagos_sighting.sql
@@ -0,0 +1,2 @@
+insert into public.costco_restock_sightings (sighted_date, item, region, location_type, set_name) values
+  ('2025-03-14', 'Terapagos UPC', 'BC', 'in-warehouse', 'Terapagos ex UPC');
